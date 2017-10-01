2c09b53586ffeecd1cda8758720e306bbc52b6e8
DeockJin Chung <jin.freestyle@gmail.com>
2017-10-01 15:31:53 +0900

    Initial

---
+++ zk_client.go
@@ -0,0 +1,242 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// @project throosea.com
+// @author DeockJin Chung (jin.freestyle@gmail.com)
+// @date 2017. 10. 1. PM 7:42
+//
+
+package support
+
+import (
+	"google.golang.org/grpc/naming"
+	"fmt"
+	"github.com/samuel/go-zookeeper/zk"
+	"google.golang.org/grpc"
+	"crypto/tls"
+	"google.golang.org/grpc/credentials"
+)
+
+func NewZkClientServant(zkIpList string) *ZkClientServant {
+	clientServant := &ZkClientServant{}
+	clientServant.addressSet = map[string]struct{}{}
+	clientServant.zkServant = NewZkServant(zkIpList)
+	clientServant.w = &serverListWatcher{
+		update:   make(chan *naming.Update, 1),
+		side:     make(chan int, 1),
+		readDone: make(chan int),
+	}
+	return clientServant
+}
+
+type ZkClientServant struct {
+	zkServant    *ZkServant
+	w    		*serverListWatcher
+	addressSet	map[string]struct{}
+	addr 		string
+}
+
+func (z *ZkClientServant) SetLogger(logger zk.Logger) *ZkClientServant {
+	z.zkServant.SetLogger(logger)
+	return z
+}
+
+func (z *ZkClientServant) Connect(znodePath string) (*grpc.ClientConn, error) {
+	err := z.zkServant.Connect()
+	if err != nil {
+		return nil, err
+	}
+
+	mode, err := z.zkServant.GetTransportMode(znodePath)
+	if err != nil {
+		return nil, err
+	}
+	if mode == TransportModeUnknown {
+		return nil, fmt.Errorf("unknown transport mode in service description : %s", znodePath)
+	}
+
+	children, ch, err := z.zkServant.ChildrenW(znodePath)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(children) == 0 {
+		return nil, fmt.Errorf("there is no server")
+	}
+	zk.DefaultLogger.Printf("initial server list : %v", children)
+	z.addr = children[0]
+
+	go func() {
+		z.watchNode(znodePath, children, ch)
+	} ()
+
+	grpc.EnableTracing = false
+
+	var gConn *grpc.ClientConn
+	switch mode {
+	case TransportModePlain :
+		gConn, err = grpc.Dial(
+			z.addr,
+			grpc.WithBlock(),
+			grpc.WithBalancer(grpc.RoundRobin(z)),
+			grpc.WithInsecure())
+	case TransportModeSsl :
+		conf := &tls.Config{
+			InsecureSkipVerify: true,
+		}
+		creds := credentials.NewTLS(conf)
+		gConn, err = grpc.Dial(
+			z.addr,
+			grpc.WithBlock(),
+			grpc.WithBalancer(grpc.RoundRobin(z)),
+			grpc.WithTransportCredentials(creds))
+	default:
+		return nil, fmt.Errorf("invalid transport mode : %v", mode)
+	}
+
+	if len(children) > 1 {
+		z.updateServerList(children)
+	}
+
+	return gConn, err
+}
+
+func (z *ZkClientServant) watchNode(znodePath string, children []string, ch <-chan zk.Event) {
+	var err error
+
+	defer func() {
+		zk.DefaultLogger.Printf("stop watching node")
+	}()
+
+	for {
+		e := <-ch
+		children, ch, err = z.zkServant.ChildrenW(znodePath)
+		if err != nil {
+			zk.DefaultLogger.Printf("zk error : %s", err.Error())
+			z.Connect(znodePath)
+			return
+		}
+
+		if e.Type & zk.EventNodeChildrenChanged != zk.EventNodeChildrenChanged {
+			continue
+		}
+
+		z.updateServerList(children)
+	}
+}
+
+func (z *ZkClientServant) updateServerList(children []string)	{
+	var updates []*naming.Update
+	updates = z.buildCreatedList(children, updates)
+	updates = z.buildRemovedList(children, updates)
+	z.w.inject(updates)
+}
+
+func (z *ZkClientServant) buildRemovedList(children []string, updates []*naming.Update) []*naming.Update {
+	copiedSet := map[string]struct{}{}
+	for key, _ := range z.addressSet {
+		copiedSet[key] = struct{}{}
+	}
+
+	for _, server := range children {
+		delete(copiedSet, server)
+	}
+
+	for server := range copiedSet {
+		updates = append(updates, &naming.Update{
+			Op:   naming.Delete,
+			Addr: server,
+		})
+		delete(z.addressSet, server)
+	}
+
+	return updates
+}
+
+
+func (z *ZkClientServant) buildCreatedList(children []string, updates []*naming.Update) []*naming.Update {
+	for _, server := range children {
+		_, ok := z.addressSet[server]
+		if !ok {
+			updates = append(updates, &naming.Update{
+				Op:   naming.Add,
+				Addr: server,
+			})
+			z.addressSet[server] = struct{}{}
+		}
+	}
+
+	return updates
+}
+
+func (z *ZkClientServant) Resolve(target string) (naming.Watcher, error) {
+	z.w.side <- 1
+	z.w.update <- &naming.Update{
+		Op:   naming.Add,
+		Addr: z.addr,
+	}
+	z.addressSet[z.addr] = struct{}{}
+	go func() {
+		<-z.w.readDone
+	}()
+	return z.w, nil
+}
+
+type serverListWatcher struct {
+	// the channel to receives name resolution updates
+	update chan *naming.Update
+	// the side channel to get to know how many updates in a batch
+	side chan int
+	// the channel to notifiy update injector that the update reading is done
+	readDone chan int
+}
+
+func (w *serverListWatcher) Next() (updates []*naming.Update, err error) {
+	n := <-w.side
+	if n == 0 {
+		return nil, fmt.Errorf("w.side is closed")
+	}
+	for i := 0; i < n; i++ {
+		u := <-w.update
+		if u != nil {
+			updates = append(updates, u)
+		}
+	}
+	w.readDone <- 0
+	return
+}
+
+func (w *serverListWatcher) Close() {
+}
+
+
+func (w *serverListWatcher) inject(updates []*naming.Update) {
+	w.side <- len(updates)
+	for _, u := range updates {
+		zk.DefaultLogger.Printf("service node updating %v : %v", geUpdateOperationName(u.Op), u.Addr)
+		w.update <- u
+	}
+	<-w.readDone
+}
+
+func geUpdateOperationName(op naming.Operation) string {
+	if op == naming.Add {
+		return "Add"
+	}
+	return "Delete"
+}
---
+++ zk_servant.go
@@ -0,0 +1,251 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// @project throosea.com
+// @author DeockJin Chung (jin.freestyle@gmail.com)
+// @date 2017. 10. 1. PM 7:42
+//
+
+package support
+
+import (
+	"github.com/samuel/go-zookeeper/zk"
+	"time"
+	"sync"
+	"strings"
+	"encoding/json"
+	"fmt"
+)
+
+
+// logging preference delivery mode
+const (
+	TransportModeUnknown = iota
+	TransportModePlain
+	TransportModeSsl
+)
+
+type TransportMode int
+
+const (
+	zkVersionAll        = -1
+	ServiceKeyTransport = "transport"
+)
+
+type ZkServant struct {
+	ipList		[]string
+	zkConn 		*zk.Conn
+	mutex		sync.Mutex
+	cond		*sync.Cond
+	state		zk.State
+	pathSet		map[string]struct{}
+}
+
+func NewZkServant(zkIpList string) *ZkServant {
+	servant := ZkServant{}
+	servant.ipList = parseIpList(zkIpList)
+	servant.mutex = sync.Mutex{}
+	servant.cond = sync.NewCond(&servant.mutex)
+	servant.state = zk.StateDisconnected
+	servant.pathSet = map[string]struct{}{}
+	return &servant
+}
+
+func (z *ZkServant) SetLogger(logger zk.Logger) *ZkServant {
+	zk.DefaultLogger = logger
+	return z
+}
+
+func (z *ZkServant) Connect() error {
+	if z.zkConn != nil {
+		return nil
+	}
+
+	conn, eventChan, err := zk.Connect(z.ipList, time.Second)
+	if err != nil {
+		return err
+	}
+	z.zkConn = conn
+
+	go func() {
+		for elem := range eventChan {
+			if !z.processZkEvent(elem) {
+				return
+			}
+		}
+	} ()
+
+	return nil
+}
+
+
+func (z *ZkServant) processZkEvent(event zk.Event) bool	{
+	zk.DefaultLogger.Printf("zk event : %v", event)
+
+	if event.Type != zk.EventSession {
+		return true
+	}
+
+	lastState := z.state
+	z.state = event.State
+
+	z.cond.Broadcast()
+
+	switch event.State {
+	case zk.StateHasSession :
+		zk.DefaultLogger.Printf("zk has session")
+		if lastState == zk.StateDisconnected {
+			z.reCreateAllEphemerals()
+		}
+	case zk.StateConnected :
+		zk.DefaultLogger.Printf("zk connected")
+	case zk.StateDisconnected :
+		zk.DefaultLogger.Printf("zk disconnected")
+	case zk.StateExpired :
+		zk.DefaultLogger.Printf("zk expired")
+		z.zkConn.Close()
+		z.zkConn = nil
+		z.Connect()
+		return false
+	}
+
+	return true
+}
+
+func (z *ZkServant) reCreateAllEphemerals() 	{
+	for k, _ := range z.pathSet {
+		err := z.createEphemeralOnce(k)
+		if err != nil {
+			zk.DefaultLogger.Printf("zk createEphemeralOnce fail : [%s] %s", k, err.Error())
+		}
+	}
+}
+
+func (z *ZkServant) ensureZkEnabled() {
+	if z.state == zk.StateHasSession {
+		return
+	}
+
+	z.mutex.Lock()
+	defer z.mutex.Unlock()
+	for z.state != zk.StateHasSession {
+		z.cond.Wait()
+	}
+}
+
+func (z *ZkServant) CreateEphemeral(path string) error {
+	zk.DefaultLogger.Printf("CreateEphemeral : %s", path)
+	z.ensureZkEnabled()
+
+	z.pathSet[path] = struct{}{}
+	acl := zk.WorldACL(zk.PermAll)
+	for {
+		_, err := z.zkConn.Create(path, nil, zk.FlagEphemeral, acl)
+		if err != nil {
+			zk.DefaultLogger.Printf("zk fail to createEphemeral : [%s] %s", path, err.Error())
+			time.Sleep(time.Second * 5)
+			continue
+		}
+		break
+	}
+
+	zk.DefaultLogger.Printf("zk create ephemeral path : %s", path)
+	return nil
+}
+
+
+func (z *ZkServant) createEphemeralOnce(path string) error {
+	zk.DefaultLogger.Printf("createEphemeralOnce : %s", path)
+	z.ensureZkEnabled()
+
+	z.pathSet[path] = struct{}{}
+	acl := zk.WorldACL(zk.PermAll)
+	_, err := z.zkConn.Create(path, nil, zk.FlagEphemeral, acl)
+	if err != nil {
+		return err
+	}
+
+	zk.DefaultLogger.Printf("zk create ephemeral path : %s", path)
+	return nil
+}
+
+func (z *ZkServant) SetData(path string, data []byte) error {
+	zk.DefaultLogger.Printf("zk setData [%s] : %d bytes", path, len(data))
+	z.ensureZkEnabled()
+	_, err := z.zkConn.Set(path, data, zkVersionAll)
+	return err
+}
+
+func (z *ZkServant) GetTransportMode(path string) (TransportMode, error) {
+	zk.DefaultLogger.Printf("GetTransportMode : %s", path)
+	b, err := z.GetData(path)
+	if err != nil {
+		return TransportModeUnknown, err
+	}
+
+	var data map[string]string
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		return TransportModeUnknown, err
+	}
+
+	// e.g {"transport":"h2c"}
+	mode, ok := data[ServiceKeyTransport]
+	if !ok {
+		return TransportModeUnknown, fmt.Errorf("Invalid service description. not found transport key")
+	}
+
+	switch(strings.ToLower(mode))	{
+	case "h2c" :
+		return TransportModePlain, nil
+	case "h2" :
+		return TransportModeSsl, nil
+	}
+
+	return TransportModeUnknown, nil
+}
+func (z *ZkServant) GetData(path string) ([]byte, error) {
+	zk.DefaultLogger.Printf("zk GetData : %s", path)
+	z.ensureZkEnabled()
+	data, _, err := z.zkConn.Get(path)
+	return data, err
+}
+
+func (z *ZkServant) ChildrenW(path string) ([]string, <-chan zk.Event, error) {
+	zk.DefaultLogger.Printf("zk ChildrenW : %s", path)
+	z.ensureZkEnabled()
+	data, _, ch, err := z.zkConn.ChildrenW(path)
+	return data, ch, err
+}
+
+func (z *ZkServant) Delete(path string) error {
+	zk.DefaultLogger.Printf("zk deleteNode [%s]", path)
+	return z.zkConn.Delete(path, zkVersionAll)
+}
+
+
+func parseIpList(config string) []string {
+	items := strings.Split(config, ",")
+
+	list := make([]string, 0)
+	for _, v := range items {
+		list = append(list, strings.Trim(v, " "))
+	}
+
+	return list
+}

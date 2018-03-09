54bc666eaa68bcf9f1fc6cf328a280051c6ed9ea
DeockJin Chung <jin.freestyle@gmail.com>
2018-03-09 16:06:46 +0900

    add grpc transaction id define

---
+++ grpc.go
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2018 SK TECHX.
+// All right reserved.
+//
+// This software is the confidential and proprietary information of SK TECHX.
+// You shall not disclose such Confidential Information and
+// shall use it only in accordance with the terms of the license agreement
+// you entered into with SK TECHX.
+//
+//
+// @project grpczk
+// @author 1100282
+// @date 2018. 3. 9. PM 4:06
+//
+
+package grpczk
+
+
+const(
+	GrpcTransactionId		= "grpczk-transaction-id"
+)
